2be575f3f8a868030e496b02be4a6e226ef96876
abolfazl <abolfazljamshidi101010@gmail.com>
2025-09-04 20:22:47 +0330

    change opacity

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
             <div className="bg-[#DF3D28B2]/70 rounded-[100px] rotate-[-25deg] w-[400px] h-[400px] absolute md:-top-14 md:-right-22 -right-60 -top-52"></div>
             <div className="bg-blue-800/50 w-[400px] h-[400px] rounded-[100px] absolute rotate-[-25deg] md:-bottom-14 md:-left-22 -bottom-60 -left-52 "></div>
             <div className="absolute top-0 left-0 w-full h-full backdrop-blur-[90px] inset-0 bg-black/10">
-                <img src="/images/Frame 28.png" alt="background" className="absolute top-0 left-0 w-full h-full opacity-80 brightness-125" />
+                <img src="/images/Frame 28.png" alt="background" className="absolute top-0 left-0 w-full h-full opacity-50" />
                 <div className="absolute top-0 left-0 w-full h-full grid grid-rows-[1fr_auto] md:py-5 py-5 px-10">
                     <div className="flex items-center justify-center">
                         <img src="/images/logo.svg" alt="logo" className="px-4 md:px-0" />

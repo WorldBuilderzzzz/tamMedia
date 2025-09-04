9134fc03747691a21ba8e707c49bfa7eaaad6aeb
abolfazl <abolfazljamshidi101010@gmail.com>
2025-09-04 19:37:29 +0330

    update sizes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,16 @@
 const Header = () => {
     return(
         <div className="h-dvh w-dvw relative overflow-hidden">
-            <div className="bg-[#DF3D28B2]/70 rounded-[100px] rotate-[-25deg] w-[400px] h-[400px] absolute -top-14 -right-22"></div>
-            <div className="bg-blue-800/50 w-[400px] h-[400px] rounded-[100px] absolute -bottom-14 -left-22 rotate-[-25deg] "></div>
+            <div className="bg-[#DF3D28B2]/70 rounded-[100px] rotate-[-25deg] w-[400px] h-[400px] absolute md:-top-14 md:-right-22 -right-60 -top-52"></div>
+            <div className="bg-blue-800/50 w-[400px] h-[400px] rounded-[100px] absolute rotate-[-25deg] md:-bottom-14 md:-left-22 -bottom-60 -left-52 "></div>
             <div className="absolute top-0 left-0 w-full h-full backdrop-blur-[90px] inset-0 bg-black/10">
                 <img src="/images/Frame 28.png" alt="background" className="absolute top-0 left-0 w-full h-full opacity-80 brightness-125" />
-                <div className="absolute top-0 left-0 w-full h-full grid grid-rows-[1fr_auto] md:py-10 py-5 px-10">
+                <div className="absolute top-0 left-0 w-full h-full grid grid-rows-[1fr_auto] md:py-5 py-5 px-10">
                     <div className="flex items-center justify-center">
                         <img src="/images/logo.svg" alt="logo" className="px-4 md:px-0" />
                     </div>
-                    <div className="flex flex-col items-center justify-center gap-20">
-                        <div className="flex items-center md:justify-around justify-between w-full font-yekan-bakh md:text-3xl text-2xl opacity-80">
+                    <div className="flex flex-col items-center justify-center gap-15">
+                        <div className="flex items-center md:justify-around justify-between w-full font-yekan-bakh md:text-2xl text-xl opacity-80">
                             <div>ارتباط با ما</div>
                             <div>پروژه‌ها</div>
                             <div>خدمات ما</div>

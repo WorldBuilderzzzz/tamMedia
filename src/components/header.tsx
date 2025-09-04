caf0b476c456c5b618bc8d7a6f6053fb447b2ba3
abolfazl <abolfazljamshidi101010@gmail.com>
2025-09-04 15:14:50 +0330

    noise background

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,12 @@
 const Header = () => {
     return(
-        //     <div className="bg-red-600/70 rounded-[500px] rotate-[-25deg] opacity-80 w-60 h-60 absolute -top-10 -right-10"></div>
-        //     <div className="bg-blue-800/70 w-60 h-60 rounded-[500px] absolute -bottom-10 -left-10 rotate-[-25deg] opacity-80 "></div>
-        //     {/* <div className="bg-white/10 backdrop-blur-3xl w-full h-full !z-40 relative"></div> */}
-        //     <div className="w-full h-full bg-gradient-to-b from-white/40 to-neutral-400/40 backdrop-blur-[100px] NOISE">
-        //         test
-        //     </div>
         <div className="h-dvh w-dvw relative overflow-hidden">
-            <div className="bg-red-600/70 rounded-[500px] rotate-[-25deg] opacity-80 w-60 h-60 absolute -top-10 -right-10"></div>
-            <div className="bg-blue-800/70 w-60 h-60 rounded-[500px] absolute -bottom-10 -left-10 rotate-[-25deg] opacity-80 "></div>
-            <div className="w-full h-full bg-white/10 absolute top-0 backdrop-blur-lg"></div>
-            {/* <img src='/backgrounds/noise.svg' alt="noise" className="" /> */}
-            <div className="noise-bg" />
+            <div className="bg-[#DF3D28B2]/70 rounded-[500px] rotate-[-25deg] w-[400px] h-[400px] absolute -top-14 -right-22 opacity-90"></div>
+            <div className="bg-blue-800/50 w-[400px] h-[400px] rounded-[500px] absolute -bottom-14 -left-22 rotate-[-25deg] opacity-90 "></div>
+            <div className="absolute top-0 left-0 w-full h-full backdrop-blur-[90px] inset-0">
+                <div className="bg-[url('/backgrounds/Frame 28.svg')] w-full h-full"></div>
+            </div>
         </div>
-        
     )
 }
 export default Header;

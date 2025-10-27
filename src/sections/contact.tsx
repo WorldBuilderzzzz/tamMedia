d71528401c5ec0948ded6e1e560f0638ec38fe0e
abolfazl <abolfazljamshidi101010@gmail.com>
2025-10-28 00:06:28 +0330

    update Contact component: adjust layout with increased padding, add RTL support for text, and enhance styling for better readability

--- src/sections/contact.tsx
+++ src/sections/contact.tsx
@@ -1,9 +1,9 @@
 import TitleGlass from "@/components/titleGlass";
 export default function Contact() {
     return(
-        <div className="h-dvh py-10 px-60 flex flex-col justify-between">
+        <div className="h-dvh py-10 px-80 flex flex-col justify-betwee gap-4">
             <TitleGlass src="/images/contact.svg" />
-            <div>
+            <div dir="rtl" className="flex-1 text-justify font-medium font-yekan-bakh text-[23px] text-black/80 leading-12">
             تام‌مدیا فقط یک آژانس بازاریابی و برندینــگ نیست؛ بلکه یک نیروی خــــــلــــاق است که بـــــرنـــدها را به‌عنوان موجــودیت‌هایی زنده و پویا می‌بیند. ما روح بـــــرنـــــــد شما را کشف کرده و آن را از طـــریق تصاویر بصـــــری جذاب، استراتـژی‌های هوشمندانه و کمپین‌های تأثیرگذار به زندگـــــی می‌آوریم. با الهام از داســـــتـــــان‌هـــــا و آرزوهــــای شما، مـــــا تجـــــربه‌هایی خـــلـــــق می‌کنیم که دیده می‌شوند، احساس می‌شوند و فراموش‌نشدنی هستند.
             </div>
             <div className="w-full flex flex-row-reverse items-center justify-between font-yekan-bakh md:text-xl text-[16px] border border-white/30 bg-[#002FFF05] backdrop-blur-[30px] px-18 py-7 rounded-full">

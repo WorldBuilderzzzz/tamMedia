bf8cb0f38a52066134560e474d1e7815dcd49c27
abolfazl <abolfazljamshidi101010@gmail.com>
2025-10-27 20:46:08 +0330

    refactor header component: remove background shapes and simplify layout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,7 @@
 const Header = () => {
     return(
         <div className="h-dvh w-dvw relative overflow-hidden">
-            <div className="bg-[#DF3D28B2]/70 rounded-[100px] rotate-[-25deg] w-[400px] h-[400px] absolute md:-top-14 md:-right-22 -right-60 -top-52"></div>
-            <div className="bg-blue-800/50 w-[400px] h-[400px] rounded-[100px] absolute rotate-[-25deg] md:-bottom-14 md:-left-22 -bottom-60 -left-52 "></div>
-            <div className="absolute top-0 left-0 w-full h-full backdrop-blur-[90px] inset-0 bg-black/10">
+            <div className="absolute top-0 left-0 w-full h-full inset-0 bg-black/10">
                 <img src="/images/noise.png" alt="background" className="absolute top-0 left-0 w-full h-full opacity-50" />
                 <div className="absolute top-0 left-0 w-full h-full grid grid-rows-[1fr_auto] md:py-5 py-5 px-10">
                     <div className="flex flex-col md:gap-10 gap-5 items-center justify-center">

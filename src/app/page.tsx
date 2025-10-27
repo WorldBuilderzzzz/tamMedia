209db85a1f546effa6dacb043986e020b03dc3e5
abolfazl <abolfazljamshidi101010@gmail.com>
2025-10-27 22:19:18 +0330

    add contact section: introduce Contact component and integrate it into the Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/sections/header";
+import Contact from "@/sections/contact";
 
 export default function Home() {
   return (
-    <div className="HOME">
+    <div className="overflow-hidden">
       <Header />
+      <Contact />
     </div>
   );
 }
---
+++ src/sections/contact.tsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+    return(
+        <div className="min-h-dvh">
+            <h1>Contact</h1>
+        </div>
+    )
+}

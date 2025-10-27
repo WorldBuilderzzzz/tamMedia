394f506598340ce82cfa782b530c38aacd492cdd
abolfazl <abolfazljamshidi101010@gmail.com>
2025-10-27 21:09:10 +0330

    update logo and tamMedia SVG files; adjust tamMedia image scale in header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
                 <div className="absolute top-0 left-0 w-full h-full grid grid-rows-[1fr_auto] md:py-5 py-5 px-10">
                     <div className="flex flex-col md:gap-10 gap-5 items-center justify-center">
                         <img src="/images/logo.svg" alt="logo" className="px-4 md:px-0 w-[400px] md:w-[550px]" />
-                        <img src="/images/tamMedia.svg" alt="tamMedia" className="px-4 md:px-0 w-[250px] md:w-[350px]" />
+                        <img src="/images/tamMedia.svg" alt="tamMedia" className="px-4 md:px-0 w-[250px] md:w-[350px] scale-150" />
                     </div>
                     <div className="flex flex-col items-center justify-center gap-15">
                         <div className="flex items-center md:justify-around justify-between w-full font-yekan-bakh md:text-2xl text-[16px] opacity-80">

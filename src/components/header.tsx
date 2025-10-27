29ca82b62cb2e8cb581f2ce14790e3c0a56949f9
abolfazl <abolfazljamshidi101010@gmail.com>
2025-10-27 21:39:26 +0330

    update header component: adjust layout, change text from "پروژه‌ها" to "نتیجه ها", and enhance styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,10 +10,10 @@ const Header = () => {
                         <img src="/images/logo.svg" alt="logo" className="px-4 md:px-0 w-[400px] md:w-[550px]" />
                         <img src="/images/tamMedia.svg" alt="tamMedia" className="px-4 md:px-0 w-[250px] md:w-[350px] scale-150" />
                     </div>
-                    <div className="flex flex-col items-center justify-center gap-15">
-                        <div className="flex items-center md:justify-around justify-between w-full font-yekan-bakh md:text-2xl text-[16px] opacity-80">
+                    <div className="flex flex-col items-center justify-center gap-4">
+                        <div className="flex items-center md:justify-around gap-40 font-yekan-bakh md:text-xl text-[16px] opacity-95 bg-[#002FFF05] backdrop-blur-[100px] px-18 py-7 rounded-full">
                             <div>ارتباط با ما</div>
-                            <div>پروژه‌ها</div>
+                            <div>نتیجه ها</div>
                             <div>خدمات ما</div>
                         </div>
                         <div className="rotate-90 text-4xl font-bold text-black font-anonymous-pro opacity-80 cursor-pointer">{'>'}</div>

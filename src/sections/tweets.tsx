efd88f2ede410906515098ffe8b85036d5688ae8
abolfazl <abolfazljamshidi101010@gmail.com>
2025-10-30 06:19:28 +0330

    Add Avatar component and utility functions; update package dependencies and add new font

--- src/sections/tweets.tsx
+++ src/sections/tweets.tsx
@@ -4,18 +4,19 @@ import { Autoplay } from 'swiper/modules';
 import 'swiper/css';
 
 import TitleGlass from "@/components/titleGlass";
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
 
 export default function Tweets () {
 
     const tweets: {id: string, title: string, description: string, instagram_id: string, image: string}[] = [
-        {id: "1", title: "خانم تام‌مدیانژاد", "instagram_id": "tamMediaNezhad", image: "/images/tweet1.jpg",  description: "همکاری با تام مدیا در پروژه تولید محتوا تجربه‌ای بسیار حرفه‌ای و رضایت‌بخش بود. تیم خلاق و متعهد تام مدیا با درک دقیق نیازهای ما، محتوایی باکیفیت و اثرگذار تولید کرد که کاملاً با اهداف پروژه هم‌راستا بود. مدیریت قوی، تحویل به‌موقع و توجه به جزئیات از ویژگی‌های برجسته این همکاری بود. بدون شک، تام مدیا انتخابی مطمئن برای پروژه‌های آینده ماست!"},
-        {id: "2", title: "خانم تام‌مدیا کلاب", "instagram_id": "tamMediaClub", image: "/images/tweet2.jpg", description: "همکاری با تام مدیا در پروژه تولید محتوا تجربه‌ای بسیار حرفه‌ای و رضایت‌بخش بود. تیم خلاق و متعهد تام مدیا با درک دقیق نیازهای ما، محتوایی باکیفیت و اثرگذار تولید کرد که کاملاً با اهداف پروژه هم‌راستا بود. مدیریت قوی، تحویل به‌موقع و توجه به جزئیات از ویژگی‌های برجسته این همکاری بود. بدون شک، تام مدیا انتخابی مطمئن برای پروژه‌های آینده ماست!"},
-        {id: "3", title: "خانم تام‌مدیانژاد", "instagram_id": "tamMediaNezhad", image: "/images/tweet3.jpg", description: "همکاری با تام مدیا در پروژه تولید محتوا تجربه‌ای بسیار حرفه‌ای و رضایت‌بخش بود. تیم خلاق و متعهد تام مدیا با درک دقیق نیازهای ما، محتوایی باکیفیت و اثرگذار تولید کرد که کاملاً با اهداف پروژه هم‌راستا بود. مدیریت قوی، تحویل به‌موقع و توجه به جزئیات از ویژگی‌های برجسته این همکاری بود. بدون شک، تام مدیا انتخابی مطمئن برای پروژه‌های آینده ماست!"},
-        {id: "4", title: "خانم تام‌مدیا کلاب", "instagram_id": "tamMediaClub", image: "/images/tweet4.jpg", description: "همکاری با تام مدیا در پروژه تولید محتوا تجربه‌ای بسیار حرفه‌ای و رضایت‌بخش بود. تیم خلاق و متعهد تام مدیا با درک دقیق نیازهای ما، محتوایی باکیفیت و اثرگذار تولید کرد که کاملاً با اهداف پروژه هم‌راستا بود. مدیریت قوی، تحویل به‌موقع و توجه به جزئیات از ویژگی‌های برجسته این همکاری بود. بدون شک، تام مدیا انتخابی مطمئن برای پروژه‌های آینده ماست!"},
+        {id: "1", title: "خانم تام‌مدیانژاد", "instagram_id": "tamMediaNezhad", image: "profile",  description: "همکاری با تام مدیا در پروژه تولید محتوا تجربه‌ای بسیار حرفه‌ای و رضایت‌بخش بود. تیم خلاق و متعهد تام مدیا با درک دقیق نیازهای ما، محتوایی باکیفیت و اثرگذار تولید کرد که کاملاً با اهداف پروژه هم‌راستا بود. مدیریت قوی، تحویل به‌موقع و توجه به جزئیات از ویژگی‌های برجسته این همکاری بود. بدون شک، تام مدیا انتخابی مطمئن برای پروژه‌های آینده ماست!"},
+        {id: "2", title: "خانم تام‌مدیا کلاب", "instagram_id": "tamMediaClub", image: "profile", description: "همکاری با تام مدیا در پروژه تولید محتوا تجربه‌ای بسیار حرفه‌ای و رضایت‌بخش بود. تیم خلاق و متعهد تام مدیا با درک دقیق نیازهای ما، محتوایی باکیفیت و اثرگذار تولید کرد که کاملاً با اهداف پروژه هم‌راستا بود. مدیریت قوی، تحویل به‌موقع و توجه به جزئیات از ویژگی‌های برجسته این همکاری بود. بدون شک، تام مدیا انتخابی مطمئن برای پروژه‌های آینده ماست!"},
+        {id: "3", title: "خانم تام‌مدیانژاد", "instagram_id": "tamMediaNezhad", image: "profile", description: "همکاری با تام مدیا در پروژه تولید محتوا تجربه‌ای بسیار حرفه‌ای و رضایت‌بخش بود. تیم خلاق و متعهد تام مدیا با درک دقیق نیازهای ما، محتوایی باکیفیت و اثرگذار تولید کرد که کاملاً با اهداف پروژه هم‌راستا بود. مدیریت قوی، تحویل به‌موقع و توجه به جزئیات از ویژگی‌های برجسته این همکاری بود. بدون شک، تام مدیا انتخابی مطمئن برای پروژه‌های آینده ماست!"},
+        {id: "4", title: "خانم تام‌مدیا کلاب", "instagram_id": "tamMediaClub", image: "profile", description: "همکاری با تام مدیا در پروژه تولید محتوا تجربه‌ای بسیار حرفه‌ای و رضایت‌بخش بود. تیم خلاق و متعهد تام مدیا با درک دقیق نیازهای ما، محتوایی باکیفیت و اثرگذار تولید کرد که کاملاً با اهداف پروژه هم‌راستا بود. مدیریت قوی، تحویل به‌موقع و توجه به جزئیات از ویژگی‌های برجسته این همکاری بود. بدون شک، تام مدیا انتخابی مطمئن برای پروژه‌های آینده ماست!"},
     ]
     
     return(
-        <div className="h-dvh py-10 flex flex-col justify-between gap-18">
+        <div className="h-dvh py-10 flex flex-col gap-40 font-yekan-bakh">
             <div className="px-60">
                 <TitleGlass title="نتیجه همکاری با" />
             </div>
@@ -24,21 +25,24 @@ export default function Tweets () {
                     className="flex-1"
                     modules={[Autoplay]}
                     loop={true}
-                    slidesPerView={3}
+                    slidesPerView={2.6}
                     spaceBetween={40}
                     autoplay={{ delay: 2000, disableOnInteraction: false }}
                 >
                     {tweets.map((tweet) => (
                         <SwiperSlide key={tweet.id}>
-                            <div className="">
-                                <div>
-                                    <div>
-                                        <img src={tweet.image} alt={tweet.title} className="w-full h-full object-cover" />
-                                        <div>{tweet.title}</div>
+                            <div className="bg-white px-8 py-4 rounded-4xl space-y-4 border border-gray-300 leading-8">
+                                <div className="flex items-center justify-between">
+                                    <div className="flex gap-2 items-center">
+                                        <Avatar className='w-13 h-13'>
+                                            <AvatarImage src={`/images/${tweet.image}.png`} alt={tweet.title} />
+                                            <AvatarFallback>CN</AvatarFallback>
+                                        </Avatar>
+                                        <div className="text-lg font-bold">{tweet.title}</div>
                                     </div>
-                                    <div>@{tweet.instagram_id}</div>
+                                    <div dir='ltr' className='text-[#7C7C7C]'>@{tweet.instagram_id}</div>
                                 </div>
-                                <div>{tweet.description}</div>
+                                <div className="font-light">{tweet.description}</div>
                             </div>
                         </SwiperSlide>
                     ))}

735d1e81358e5da9608b1567ffdc8b12b05e95fb
abolfazl <abolfazljamshidi101010@gmail.com>
2025-11-05 17:46:26 +0330

    Refactor background elements in Home component: replace SVG images with styled divs for enhanced visual design and responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,23 @@ import Header from "@/sections/header";
 import Contact from "@/sections/contact";
 import Tweets from "@/sections/tweets";
 
+const radiusShape = '60% 40% 0% 100% / 52% 100% 0% 48%'
+
 export default function Home() {
   return (
     <div className="h-dvh w-dvw relative overflow-hidden">
       <div className="absolute top-0 left-0 w-full h-full inset-0 bg-black/10">
           <img src="/images/noise.png" alt="background" className="absolute top-0 left-0 w-full h-full opacity-50" />
-          <img src="/images/redEllipse.svg" alt="redEllipse" className="absolute -top-10 -left-10" />
-          <img src="/images/blueEllipse.svg" alt="blueEllipse" className="absolute -bottom-10 -right-10" />
+          <div className="absolute -top-10 -left-10 w-[300px] h-[300px] scale-200 bg-[#DF3D28] rotate-[140deg]" style={{borderRadius: radiusShape,
+                rotate: '207deg',
+                top: '-250px',
+                left: '-40px',
+          }}></div>
+          <div className="absolute -bottom-10 -right-10 w-[300px] h-[300px] scale-200 bg-[#0205AD]" style={{borderRadius: radiusShape,
+                rotate: '14deg',
+                bottom: '-217px',
+                right: '16px',
+          }}></div>
           <div dir="ltr" className="overflow-y-auto scroll-smooth h-full absolute top-0 left-0 w-full">
             <div dir="rtl">
               <Header />

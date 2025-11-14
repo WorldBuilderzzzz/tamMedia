4ed88e931f0006344cbd200bf43a0ea0708971df
abolfazl <abolfazljamshidi101010@gmail.com>
2025-11-14 17:24:23 +0330

    fix: implement responsive design adjustments in Home component for improved layout on larger screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,24 @@ import Header from "@/sections/header";
 import Contact from "@/sections/contact";
 import Tweets from "@/sections/tweets";
 import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
 
 const radiusShape = '60% 40% 0% 100% / 52% 100% 0% 48%'
 
 export default function Home() {
+  const [isMdOrLarger, setIsMdOrLarger] = useState(false);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsMdOrLarger(window.innerWidth >= 768);
+    };
+
+    checkScreenSize();
+    window.addEventListener('resize', checkScreenSize);
+
+    return () => window.removeEventListener('resize', checkScreenSize);
+  }, []);
+
   return (
     <div className="h-dvh w-dvw relative overflow-hidden">
       <div className="absolute top-0 left-0 w-full h-full inset-0 bg-black/10">
@@ -19,14 +33,20 @@ export default function Home() {
             className="absolute w-[148px] h-[100px] md:w-[300px] md:h-[300px] scale-200 bg-[#DF3D28]"
             style={{
               borderRadius: radiusShape,
-              rotate: '174deg',
-              top: '-5px',
-              left: '-29px',
+              ...(isMdOrLarger ? {
+                rotate: '207deg',
+                top: '-300px',
+                left: '-40px',
+              } : {
+                rotate: '174deg',
+                top: '-5px',
+                left: '-29px',
+              })
             }}
             initial={{ scale: 0, opacity: 0 }}
-              animate={{
+            animate={{
               scale: 1,
-              rotate: '174deg',
+              rotate: isMdOrLarger ? '207deg' : '174deg',
               opacity: 1,
             }}
             transition={{
@@ -39,9 +59,15 @@ export default function Home() {
             className="absolute -bottom-[120px] -right-[70px] w-[180px] h-[180px] md:w-[300px] md:h-[300px] scale-200 bg-[#0205AD]"
             style={{
               borderRadius: radiusShape,
-              rotate: '14deg',
-              bottom: '-120px',
-              right: '-70px',
+              ...(isMdOrLarger ? {
+                rotate: '14deg',
+                bottom: '-300px',
+                right: '16px',
+              } : {
+                rotate: '14deg',
+                bottom: '-120px',
+                right: '-70px',
+              })
             }}
             initial={{ scale: 0, opacity: 0 }}
             animate={{

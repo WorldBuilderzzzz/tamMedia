f90df7bd6045ce3c3ebfeca39416ba59122553c8
abolfazl <abolfazljamshidi101010@gmail.com>
2025-11-14 17:05:46 +0330

    fix: update transition duration in Tweets component for enhanced animation effect

--- src/sections/tweets.tsx
+++ src/sections/tweets.tsx
@@ -42,7 +42,7 @@ export default function Tweets () {
                 initial={{ opacity: 0, y: -100 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 viewport={{ once: true, amount: 0.3 }}
-                transition={{ duration: 0, delay: 0.2, ease: [0.22, 1, 0.36, 1] }}
+                transition={{ duration: .8, delay: 0.2, ease: [0.22, 1, 0.36, 1] }}
             >
                 <Swiper
                     className='h-full my-5'

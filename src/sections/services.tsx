97d8ba1d8588442ba72c955fb07ac917255fdba9
abolfazl <abolfazljamshidi101010@gmail.com>
2025-11-23 02:20:31 +0330

    feat: add arrow SVG icon and enhance Services section with service selection functionality

--- src/sections/services.tsx
+++ src/sections/services.tsx
@@ -1,7 +1,20 @@
 import TitleGlass from "@/components/titleGlass";
-import { Plus } from 'lucide-react';
+import { ArrowLeft, Plus } from 'lucide-react';
 import { motion } from "framer-motion";
+import { useState } from "react";
+
+type serviceType = 'service' | 'guide' | 'consultation'
+
+
 export default function Services() {
+    const [service, setService] = useState<serviceType | null>(null)
+
+    const serviceMap: Record<serviceType, string> = {
+        "service": "خـــدمـــــــات",
+        "guide": "آمـــــــوزش",
+        "consultation": "مـــــشــــاوره"
+    }
+    
     const scrollTo = (id: string) => {
         const el = document.getElementById(id);
         if (el) el.scrollIntoView({ behavior: 'smooth', block: 'start' });
@@ -24,25 +37,42 @@ export default function Services() {
                 <div className="px-6 md:px-80">
                     <TitleGlass title="خدمات ما" />
                 </div>
-                <div className="w-full h-full flex flex-col gap-4 justify-around items-center bg-[#193FE905] backdrop-blur-[20px] rounded-3xl p-5 font-yekan-bakh">
-                    <div className="flex gap-5 text-4xl w-full">
-                        <div className="flex-1 text-left">
-                            <Plus className="mr-auto"/>
+                <div className="w-full h-full bg-[#193FE905] backdrop-blur-[20px] rounded-3xl p-5 font-yekan-bakh">
+                    {service == null ? (
+                        <div className="flex flex-col gap-4 justify-around items-center h-full">
+                            <div className="flex gap-5 text-4xl w-full">
+                                <div className="flex-1 text-left">
+                                    <Plus className="mr-auto"/>
+                                </div>
+                                <span className="flex-1 text-right font-semibold cursor-pointer" onClick={() => setService('service')}>خـــدمـــــــات </span>
+                            </div>
+                            <div className="flex gap-5 text-4xl w-full">
+                                <div className="flex-1 text-left">
+                                    <Plus className="mr-auto text-[#0205B6]"/>
+                                </div>
+                                <span className="flex-1 text-right font-semibold cursor-pointer" onClick={() => setService('guide')}>آمـــــــوزش </span>
+                            </div>
+                            <div className="flex gap-5 text-4xl w-full">
+                                <div className="flex-1 text-left">
+                                    <Plus className="mr-auto text-[#DF3D28]"/>
+                                </div>
+                                <span className="flex-1 text-right font-semibold cursor-pointer" onClick={() => setService('consultation')}>مـــــشــــاوره </span>
+                            </div>
                         </div>
-                        <span className="flex-1 text-right font-semibold">خـــدمـــــــات </span>
-                    </div>
-                    <div className="flex gap-5 text-4xl w-full">
-                        <div className="flex-1 text-left">
-                            <Plus className="mr-auto text-[#0205B6]"/>
+                    ) : (
+                        <div className="">
+                            <div className="flex justify-between items-center">
+                                <div className="font-semibold text-[45px]">{serviceMap[service]}</div>
+                                <div className="flex items-center gap-4">
+                                    <span className="text-[25px] cursor-pointer" onClick={() => setService(null)}> سرویس ها</span>
+                                    <img src="/images/arrow.svg" alt="arrow icon" className="w-8" />
+                                </div>
+                            </div>
+                            <div>
+                                
+                            </div>
                         </div>
-                        <span className="flex-1 text-right font-semibold">آمـــــــوزش </span>
-                    </div>
-                    <div className="flex gap-5 text-4xl w-full">
-                        <div className="flex-1 text-left">
-                            <Plus className="mr-auto text-[#DF3D28]"/>
-                        </div>
-                        <span className="flex-1 text-right font-semibold">مـــــشــــاوره </span>
-                    </div>
+                    )}
                 </div>
             </motion.div>
             <div
